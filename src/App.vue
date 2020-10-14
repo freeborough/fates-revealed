b945382a4ff54e6af4ccf4594711db8de31cec94
freeborough <andy@freeborough.com>
2020-10-14 17:02:30 +0100

    Created a simple GiverList component.
    This component uses the game data and giver symbol images to produce a list of all the givers, and each giver can be toggled on/off.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
     <h1>Fates Revealed</h1>
+    <GiverList></GiverList>
   </div>
 </template>
 
 <script>
+import GiverList from './components/GiverList';
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    GiverList
+  }
 }
 </script>
+
+<style>
+
+body {
+  background-color: #191919;
+  color: #8c8c8c;
+  font-family: sans-serif;
+}
+
+</style>
---
+++ src/components/GiverList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="giver-list">
+    <button @click="toggle(giver)" v-for="giver in givers" :key="giver.name" class="giver-toggle" :class="{ 'giver-selected': giver.selected }">
+      <img class="giver-symbol" :src="giver.symbol" :alt="giver.name" />
+      <span class="giver-name">{{ giver.name }}</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import { givers } from '../GameData';
+
+export default {
+  name: 'giver-list',
+  data: function() {
+    return {
+      givers: givers.map(function(giver) {
+        return {
+          name: giver,
+          symbol: `/images/giver-symbols/${giver}.png`,
+          selected: false
+        }})
+    }
+  },
+  methods: {
+    toggle(giver) {
+      giver.selected = !giver.selected;
+    }
+  }
+}
+</script>
+
+<style>
+.giver-symbol {
+  height: 100%;
+  filter: saturate(50%);
+}
+
+.giver-toggle {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 8vh;
+  font-size: 4vh;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.giver-name {
+  color: #8c8c8c;
+  padding-left: 0.3em;
+  flex-grow: 1;
+}
+
+.giver-selected .giver-name {
+  color: #ffffff;
+  text-decoration: underline;
+}
+
+.giver-selected .giver-symbol {
+  filter: saturate(100%);
+}
+</style>
